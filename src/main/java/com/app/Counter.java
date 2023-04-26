8e2d8a310b4c57eaeb3d3ca38d4be30f5061229c
Mendes <lfelipecmendes@icloud.com>
2023-04-26 11:47:50 -0300

    added the first challenge to Counter.java

---
+++ src/main/java/com/app/Counter.java
@@ -0,0 +1,14 @@
+package com.app;
+
+public class Counter {
+  public int countSheeps(Boolean[] arrayOfSheeps) {
+    // Consider an array/list of sheep where some sheep may be missing from their
+    // place. We need a function that counts the number of sheep present in the
+    // array (true means present).
+    int count = 0;
+
+    // Coloque seu código aqui.
+
+    return count;
+  }
+}
